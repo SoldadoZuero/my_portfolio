57c2a325168369935ffe2c02c7cde7149b4e7c49
SoldadoZuero <skeleton_fake@outlook.com>
2024-09-09 16:09:29 -0300

    Finished about me panel polishing

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -2,11 +2,17 @@
     function aboutContainerChange() {
         var scrollLimit = 180;
         var aboutContainer = document.getElementById('aboutme');
+        var aboutMeButton = document.getElementById('aboutme_button');
+        var aboutPanel = document.getElementById('project_menu');
 
         if (window.scrollY >= scrollLimit) {
             aboutContainer.classList.remove('about_panel_hidden');
+            aboutMeButton.classList.remove('about_panel_hidden');
+            aboutPanel.classList.remove('about_panel_hidden');
         } else {
             aboutContainer.classList.add('about_panel_hidden');
+            aboutMeButton.classList.add('about_panel_hidden');
+            aboutPanel.classList.add('about_panel_hidden');
         }
 
     }
