16aa7a625e6bf300836c98c2e263bb3b9b64fdaf
SoldadoZuero <skeleton_fake@outlook.com>
2024-11-25 17:32:05 -0300

    Projects are now always showing as panels

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -13,37 +13,3 @@
 
     window.addEventListener('scroll', aboutContainerChange);
 })();
-
-(document.addEventListener('DOMContentLoaded', ()=> {
-    var projectPanel = document.getElementById('project_panel');
-    var projectMenu = document.getElementById('project_menu');
-    var aboutButton = document.getElementById('aboutme_button')
-    const menuOpen = document.querySelector('.project_panel_open')
-
-    const projectPanelShowClass = 'project_panel_open';
-    const projectPanelHiddenClass = 'project_panel_close';
-    const projectMenuShowClass = 'project_menu_open';
-    const buttonAboutHiddenClass = 'button_about_menu_open'
-
-    const toggleClass = (element, className) => {
-        element.style.transition = 'all 1s cubic-bezier(0.65, 0.05, 0.36, 1)';
-        element.classList.toggle(className);
-    };
-
-    function menuOpening () {
-        if (menuOpen) {
-            toggleClass(projectPanel, projectPanelHiddenClass);
-            toggleClass(aboutButton, buttonAboutHiddenClass);
-            toggleClass(projectPanel, projectPanelShowClass);
-            toggleClass(projectMenu, projectMenuShowClass);
-        } else {
-            toggleClass(projectMenu, projectMenuShowClass);
-            toggleClass(projectPanel, projectPanelHiddenClass);
-            toggleClass(aboutButton, buttonAboutHiddenClass);
-            toggleClass(projectPanel, projectPanelShowClass);
-        }
-    };
-
-    aboutButton.removeEventListener('click', menuOpening);
-    aboutButton.addEventListener('click', menuOpening);
-}))
