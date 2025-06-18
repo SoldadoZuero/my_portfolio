458629638d348b14fd33fb99cbeb0bf009ed339e
SoldadoZuero <skeleton_fake@outlook.com>
2025-06-18 15:21:27 -0300

    Created a toml and now working with UV

--- principal/views.py
+++ principal/views.py
@@ -6,10 +6,6 @@ from django.views.generic import TemplateView
 class PrincipalHome(TemplateView):
     template_name = 'principal/pages/home.html'
 
-<<<<<<< HEAD
-# class
-=======
 
 class PrincipalProjects(TemplateView):
     template_name = 'principal/pages/projects.html'
->>>>>>> ef05d5e5d53a79abfdc30e9d5b97e8ab5504fbbb
